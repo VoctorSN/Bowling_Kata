31c93287dc216df27bf94fb3763c0075cbbc2142
Victor Clase <victorsancheznogueira@gmail.com>
2024-01-29 10:44:15 +0100

    feat: :sparkles: improving module Calculate_score

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 
 class ScoreCard:
-    def __init__(score:str):
-        self.score=score
+    INITIAL_SCORE = 0
+    def __init__(self,throws:str):
+        self.throws = throws
+
     def Calculate_Score(self):
-        
+        score = self.INITIAL_SCORE
+        for throw,points in enumerate(self.throws):
+            
