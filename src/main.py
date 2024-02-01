4784dcc4a94331aa1bd1bd5b8a5a1d46f19f6e49
Victor Clase <victorsancheznogueira@gmail.com>
2024-02-01 11:21:14 +0100

    refactor: :art: redifining variables and modules

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 
 class ScoreCard:
-    PUNTUATION = {
+    INITIAL_GAME_SCORE = 0
+    SCORE_BY_PINS = {
         '1':1,
         '2':2,
         '3':3,
@@ -13,55 +14,59 @@ class ScoreCard:
         'X':10,
         '/':10,
         '-':0
-    } 
-    def __init__(self,throws:str):
-        self.throws = throws
+    }
+    def __init__(self,game:str):
+        self.game_score = self.INITIAL_GAME_SCORE
+        self.game = game
 
     def __repr__(self) -> str:
-        return f"{self.throws}"
+        return f"{' , '.join(self.stylizer())}"
 
     def stylizer(self) -> list:
-        throws_listed = []
-        throws = self.throws.replace('X','XX')
-        if len(throws) == 21:
-            throws += '-'
-        for position,throw in enumerate(throws):
+        pins = []
+        game = self.game.replace('X','XX')
+        if len(game) == 21:
+            game += '-'
+        for position,throw in enumerate(game):
             if position%2 == 0:
                 continue
             if throw == 'X':
-                throws_listed.append(throw)
+                pins.append(throw)
                 continue
-            throws_listed.append(throws[position-1] + throw)
-        return throws_listed
+            pins.append(game[position-1] + throw)
+        return pins
 
-    def Calcutale_frame(self,frame:str) -> int :
+    def Calcutale_frame_punctuation(self,frame:str) -> int :
+        first_roll = frame[0]
         if len(frame) == 1:
-            return self.PUNTUATION[frame]
+            return self.SCORE_BY_PINS[first_roll]
+        second_roll = frame[1]
         if frame[1] == '/':
-            return self.PUNTUATION[frame[1]]
-        return self.PUNTUATION[frame[0]] + self.PUNTUATION[frame[1]]
+            return self.SCORE_BY_PINS[second_roll]
+        return self.SCORE_BY_PINS[first_roll] + self.SCORE_BY_PINS[second_roll]
 
     def Calculate_Score(self) -> int:
-        throws = self.stylizer()
-        score = 0
-        for position,frame in enumerate(throws[:10]):
-            score += self.Calcutale_frame(frame)
-            if frame[0] == 'X' or frame[1] == '/':
-                try:
-                    score += self.Calcutale_frame(throws[position+1][0])
-                except Exception:
-                    continue
-            if frame[0] == 'X':
-                try:
-                    if throws[position+1] == 'X':
-                        score += self.Calcutale_frame(throws[position+2][0])
-                    else:
-                        score += self.Calcutale_frame(throws[position+1][1])
-                        if throws[position+1][1] == '/' :
-                            score -= self.Calcutale_frame(throws[position+1][0])
-                except Exception:
-                    continue
-        return score
+        game = self.stylizer()
+        for position,frame in enumerate(game[:10]):
+            if self.is_frame_a_strike_or_spare(frame):
+                self.calculate_strike_or_spare(game,frame,position)
+            else:
+                self.game_score += self.Calcutale_frame_punctuation(frame)
+        return self.game_score
+
+    def is_frame_a_strike_or_spare(self,frame:str) -> bool:
+        return frame[0] == 'X' or frame[1] == '/'
+
+    def calculate_strike_or_spare(self,game:list,frame:str,position:int) -> None:
+        self.game_score += self.Calcutale_frame_punctuation(frame) + self.Calcutale_frame_punctuation(game[position+1][0])
+        if frame[0] == 'X':
+            if game[position+1] == 'X':
+                self.game_score += self.Calcutale_frame_punctuation(game[position+2][0])
+                return None
+            self.game_score += self.Calcutale_frame_punctuation(game[position+1][1])
+            if game[position+1][1] == '/' :
+                self.game_score -= self.Calcutale_frame_punctuation(game[position+1][0])
+        return None
 
 
 if __name__ == "__main__":
